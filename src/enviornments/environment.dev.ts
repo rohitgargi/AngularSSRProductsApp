ea059228c3dfc3e7dc3d68e44008384edf7d4923
Rohit Sharma <rohitgargi0293@gmail.com>
2023-01-03 15:43:54 +0530

    Create environment.dev.ts

---
+++ src/enviornments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    title:"Dev application title",
+    serverUrl:'https://webapi-node-only.cyclic.app/api'
+    //serverUrl:'http://localhost:5000/api'
+  };
+  
+  // serverUrl:'https://webapi-node-only.cyclic.app/api'
