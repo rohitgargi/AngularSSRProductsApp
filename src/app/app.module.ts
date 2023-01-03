b8364b9567fc5b40e9d76b56edca103f3ae7e481
Rohit Sharma <rohitgargi0293@gmail.com>
2023-01-03 15:25:57 +0530

    Create app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { HeaderComponent } from './core/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    HeaderComponent,
+
+    
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
