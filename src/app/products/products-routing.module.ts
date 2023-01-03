4b9a0bc89faa5b97f6fd4cb74560fbe5250595ed
Rohit Sharma <rohitgargi0293@gmail.com>
2023-01-03 15:35:50 +0530

    Create products-routing.module.ts

---
+++ src/app/products/products-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import {  RouterModule, Routes } from "@angular/router";
+import { ProductListComponent } from "./product-list.component";
+
+
+const routes: Routes = [
+    {
+        path:'',
+        component:ProductListComponent,
+        pathMatch:'full',
+        
+    }
+]
+
+@NgModule({
+    imports:[RouterModule.forChild(routes)]
+})
+
+export class ProductsRoutingModule{
+    
+}
